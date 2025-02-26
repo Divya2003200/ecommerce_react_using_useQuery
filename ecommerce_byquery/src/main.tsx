4853e108c2b8a66f443c4d27c2d6c1368b56ca5f
Divya___verekar <136955617+Divya2003200@users.noreply.github.com>
2025-02-26 11:04:40 +0530

    Update main.tsx

--- ecommerce_byquery/src/main.tsx
+++ ecommerce_byquery/src/main.tsx
@@ -1,12 +1,11 @@
  
-// In your main application file (e.g., index.tsx)
+ 
 import React from 'react';
 import ReactDOM from 'react-dom/client'; 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import App from './App'; // Assuming App is your main component
+import App from './App';  
 import {BrowserRouter as Router} from 'react-router-dom'
-
-// Create a new QueryClient instance
+ 
 const queryClient = new QueryClient(); 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
